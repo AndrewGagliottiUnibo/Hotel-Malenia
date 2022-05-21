7f95a06daa4c04e694f632c5796d75ad09dd52dd
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-21 12:10:44 +0200

    login page design 1.0

--- ProgettoDB/src/db/pages/LoginPages.java
+++ ProgettoDB/src/db/pages/LoginPages.java
@@ -15,29 +15,92 @@ import javax.swing.JTextField;
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class LoginPages {
 
 	JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 	
 	/**
 	 * Create the application.
 	 */
 	public LoginPages() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.WHITE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		frame.setBackground(Color.YELLOW);
+		frame.setBounds(100, 100, 448, 272);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
 		
-		JPanel panel_1 = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
-		flowLayout.setAlignment(FlowLayout.LEFT);
-		frame.getContentPane().add(panel_1, BorderLayout.NORTH);
-		frame.setBackground(Color.YELLOW);
-		frame.setBounds(100, 100, 455, 312);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel lblNewLabel = new JLabel("Hotel Malenia");
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setBackground(Color.RED);
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 30));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.weighty = 0.1;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 1;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Codice");
+		lblNewLabel_2.setForeground(Color.ORANGE);
+		lblNewLabel_2.setFont(new Font("Verdana", Font.BOLD, 14));
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 4;
+		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 4;
+		panel.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		lblNewLabel_1.setForeground(Color.ORANGE);
+		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 14));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 6;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 6;
+		panel.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Accedi");
+		btnNewButton.setBackground(Color.DARK_GRAY);
+		btnNewButton.setForeground(Color.ORANGE);
+		btnNewButton.setFont(new Font("Verdana", Font.BOLD, 14));
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.weighty = 0.2;
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 8;
+		panel.add(btnNewButton, gbc_btnNewButton);
 		this.initialize();
 	}
 
