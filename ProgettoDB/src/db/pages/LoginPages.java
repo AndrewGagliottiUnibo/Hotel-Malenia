0704b70019d93a97e732a22c67a124143dc4acca
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-23 22:11:22 +0200

    new design for login and new guis

--- ProgettoDB/src/db/pages/LoginPages.java
+++ ProgettoDB/src/db/pages/LoginPages.java
@@ -24,76 +24,47 @@ public class LoginPages {
 	public LoginPages() {
 		frame = new JFrame();
 		frame.setBackground(Color.YELLOW);
-		frame.setBounds(100, 100, 448, 272);
+		frame.setBounds(100, 100, 945, 528);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		panel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Hotel Malenia");
-		lblNewLabel.setForeground(Color.RED);
-		lblNewLabel.setBackground(Color.RED);
-		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 30));
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.weighty = 0.1;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 1;
-		panel.add(lblNewLabel, gbc_lblNewLabel);
+		JLabel title = new JLabel("Hotel Malenia");
+		title.setForeground(Color.RED);
+		title.setFont(new Font("Verdana", Font.BOLD, 33));
+		title.setBounds(69, 38, 322, 157);
+		panel.add(title);
 		
-		JLabel lblNewLabel_2 = new JLabel("Codice");
-		lblNewLabel_2.setForeground(Color.ORANGE);
-		lblNewLabel_2.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_2.gridx = 0;
-		gbc_lblNewLabel_2.gridy = 4;
-		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		JLabel password = new JLabel("Password");
+		password.setForeground(Color.ORANGE);
+		password.setFont(new Font("Verdana", Font.BOLD, 18));
+		password.setBounds(130, 305, 132, 42);
+		panel.add(password);
 		
-		textField_1 = new JTextField();
-		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.gridx = 1;
-		gbc_textField_1.gridy = 4;
-		panel.add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNewLabel_1 = new JLabel("Password");
-		lblNewLabel_1.setForeground(Color.ORANGE);
-		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 6;
-		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		JLabel code = new JLabel("Codice");
+		code.setForeground(Color.ORANGE);
+		code.setFont(new Font("Verdana", Font.BOLD, 18));
+		code.setBounds(130, 217, 132, 42);
+		panel.add(code);
 		
 		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 6;
-		panel.add(textField, gbc_textField);
+		textField.setBounds(356, 232, 303, 20);
+		panel.add(textField);
 		textField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Accedi");
-		btnNewButton.setBackground(Color.DARK_GRAY);
-		btnNewButton.setForeground(Color.ORANGE);
-		btnNewButton.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.weighty = 0.2;
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton.gridx = 0;
-		gbc_btnNewButton.gridy = 8;
-		panel.add(btnNewButton, gbc_btnNewButton);
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(356, 320, 303, 20);
+		panel.add(textField_1);
+		
+		JButton loginBtn = new JButton("Login");
+		loginBtn.setForeground(Color.ORANGE);
+		loginBtn.setBackground(Color.DARK_GRAY);
+		loginBtn.setBounds(722, 422, 89, 23);
+		panel.add(loginBtn);
 		this.initialize();
 	}
 
@@ -103,5 +74,4 @@ public class LoginPages {
 	private void initialize() {
 
 	}
-
 }
