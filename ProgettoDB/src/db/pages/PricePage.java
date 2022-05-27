3e948ab773e3c1e751faf287995b3524713d6660
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-27 20:29:03 +0200

    tables must be set in both

--- ProgettoDB/src/db/pages/PricePage.java
+++ ProgettoDB/src/db/pages/PricePage.java
@@ -4,7 +4,6 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.SwingConstants;
