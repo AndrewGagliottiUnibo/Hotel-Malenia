de679e2b31c3ff7858bbc8f1892ca6a90fe723d4
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-29 10:45:41 +0200

    registration gui

---
+++ ProgettoDB/src/db/pages/ClientsInHotel.java
@@ -0,0 +1,108 @@
+package db.pages;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+
+public class ClientsInHotel {
+
+	private JFrame frmClientiInStruttura;
+	private JTable table;
+	private JTextField textField;
+
+	/**
+	 * Create the application.
+	 */
+	public ClientsInHotel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmClientiInStruttura = new JFrame();
+		frmClientiInStruttura.getContentPane().setBackground(Color.DARK_GRAY);
+		frmClientiInStruttura.setTitle("Clienti in struttura");
+		frmClientiInStruttura.setBounds(100, 100, 945, 528);
+		frmClientiInStruttura.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmClientiInStruttura.getContentPane().setLayout(null);
+		
+		JLabel title = new JLabel("Clienti attualmente presenti in struttura");
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setForeground(Color.RED);
+		title.setFont(new Font("Verdana", Font.BOLD, 18));
+		title.setBackground(Color.DARK_GRAY);
+		title.setBounds(0, 0, 929, 23);
+		frmClientiInStruttura.getContentPane().add(title);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 25, 809, 453);
+		frmClientiInStruttura.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				"Codice scheda", "Numero camera", "Cognome"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table.setFont(new Font("Monospaced", Font.PLAIN, 13));
+		scrollPane.setViewportView(table);
+		
+		JButton logout = new JButton("Logout");
+		logout.setForeground(Color.ORANGE);
+		logout.setFont(new Font("Verdana", Font.BOLD, 12));
+		logout.setBackground(Color.DARK_GRAY);
+		logout.setBounds(829, 455, 89, 23);
+		frmClientiInStruttura.getContentPane().add(logout);
+		
+		JButton showAllBtn = new JButton("Vedi");
+		showAllBtn.setForeground(Color.ORANGE);
+		showAllBtn.setFont(new Font("Verdana", Font.BOLD, 12));
+		showAllBtn.setBackground(Color.DARK_GRAY);
+		showAllBtn.setBounds(830, 25, 89, 23);
+		frmClientiInStruttura.getContentPane().add(showAllBtn);
+		
+		JLabel codeLabel = new JLabel("Codice");
+		codeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		codeLabel.setForeground(Color.ORANGE);
+		codeLabel.setFont(new Font("Verdana", Font.BOLD, 12));
+		codeLabel.setBackground(Color.ORANGE);
+		codeLabel.setBounds(829, 114, 90, 14);
+		frmClientiInStruttura.getContentPane().add(codeLabel);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Verdana", Font.BOLD, 12));
+		textField.setForeground(Color.PINK);
+		textField.setBackground(Color.BLACK);
+		textField.setBounds(829, 139, 89, 20);
+		frmClientiInStruttura.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnScheda = new JButton("Scheda");
+		btnScheda.setForeground(Color.ORANGE);
+		btnScheda.setFont(new Font("Verdana", Font.BOLD, 12));
+		btnScheda.setBackground(Color.DARK_GRAY);
+		btnScheda.setBounds(829, 170, 89, 23);
+		frmClientiInStruttura.getContentPane().add(btnScheda);
+	}
+}
--- ProgettoDB/src/db/pages/RegistrationCard.java
+++ ProgettoDB/src/db/pages/RegistrationCard.java
@@ -8,9 +8,9 @@ import java.awt.Font;
 import javax.swing.JRadioButton;
 import javax.swing.JButton;
 
-public class ClientCard {
+public class RegistrationCard {
 
-	private JFrame frame;
+	private JFrame frmRegistraCliente;
 	private JTextField name;
 	private JTextField cfField;
 	private JLabel surnameRegistered;
@@ -29,9 +29,6 @@ public class ClientCard {
 	private JRadioButton rdbtnIntCompleto;
 	private JRadioButton rdbtnIntAllInclusive;
 	private JLabel lblTipoSoggiorno;
-	private JTextField resField;
-	private JLabel lblResoconto;
-	private JButton btnAggiorna;
 
 	/**
 	 * Launch the application.
@@ -39,7 +36,7 @@ public class ClientCard {
 	/**
 	 * Create the application.
 	 */
-	public ClientCard() {
+	public RegistrationCard() {
 		initialize();
 	}
 
@@ -47,220 +44,195 @@ public class ClientCard {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setTitle("Scheda Cliente");
-		frame.getContentPane().setBackground(Color.DARK_GRAY);
-		frame.setBounds(100, 100, 945, 528);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmRegistraCliente = new JFrame();
+		frmRegistraCliente.setTitle("Registra Cliente");
+		frmRegistraCliente.getContentPane().setBackground(Color.DARK_GRAY);
+		frmRegistraCliente.setBounds(100, 100, 945, 528);
+		frmRegistraCliente.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmRegistraCliente.getContentPane().setLayout(null);
 		
 		JLabel nameRegistered = new JLabel("Nome");
 		nameRegistered.setForeground(Color.ORANGE);
 		nameRegistered.setFont(new Font("Verdana", Font.BOLD, 12));
 		nameRegistered.setBounds(10, 70, 86, 14);
-		frame.getContentPane().add(nameRegistered);
+		frmRegistraCliente.getContentPane().add(nameRegistered);
 		
 		name = new JTextField();
 		name.setBounds(10, 92, 86, 20);
-		frame.getContentPane().add(name);
+		frmRegistraCliente.getContentPane().add(name);
 		name.setColumns(10);
 		
-		JLabel codeCard = new JLabel("");
+		JLabel codeCard = new JLabel("Dati personali");
 		codeCard.setForeground(Color.RED);
 		codeCard.setFont(new Font("Verdana", Font.BOLD, 18));
 		codeCard.setBounds(10, 11, 333, 48);
-		frame.getContentPane().add(codeCard);
+		frmRegistraCliente.getContentPane().add(codeCard);
 		
 		cfField = new JTextField();
 		cfField.setColumns(10);
 		cfField.setBounds(10, 163, 190, 20);
-		frame.getContentPane().add(cfField);
+		frmRegistraCliente.getContentPane().add(cfField);
 		
 		JLabel cf = new JLabel("Codice Fiscale");
 		cf.setForeground(Color.ORANGE);
 		cf.setFont(new Font("Verdana", Font.BOLD, 12));
 		cf.setBounds(10, 141, 190, 14);
-		frame.getContentPane().add(cf);
+		frmRegistraCliente.getContentPane().add(cf);
 		
 		surnameRegistered = new JLabel("Cognome");
 		surnameRegistered.setForeground(Color.ORANGE);
 		surnameRegistered.setFont(new Font("Verdana", Font.BOLD, 12));
 		surnameRegistered.setBounds(114, 70, 86, 14);
-		frame.getContentPane().add(surnameRegistered);
+		frmRegistraCliente.getContentPane().add(surnameRegistered);
 		
 		amministrativeData = new JLabel("Dati amministrativi");
 		amministrativeData.setForeground(Color.RED);
 		amministrativeData.setFont(new Font("Verdana", Font.BOLD, 18));
 		amministrativeData.setBounds(395, 11, 209, 48);
-		frame.getContentPane().add(amministrativeData);
+		frmRegistraCliente.getContentPane().add(amministrativeData);
 		
 		surname = new JTextField();
 		surname.setColumns(10);
 		surname.setBounds(114, 92, 86, 20);
-		frame.getContentPane().add(surname);
+		frmRegistraCliente.getContentPane().add(surname);
 		
 		JLabel birthday = new JLabel("Data di nascita");
 		birthday.setForeground(Color.ORANGE);
 		birthday.setFont(new Font("Verdana", Font.BOLD, 12));
 		birthday.setBounds(10, 213, 190, 14);
-		frame.getContentPane().add(birthday);
+		frmRegistraCliente.getContentPane().add(birthday);
 		
 		dateField = new JTextField();
 		dateField.setColumns(10);
 		dateField.setBounds(10, 235, 190, 20);
-		frame.getContentPane().add(dateField);
+		frmRegistraCliente.getContentPane().add(dateField);
 		
 		JLabel lblInizioSoggiorno = new JLabel("Inizio soggiorno");
 		lblInizioSoggiorno.setForeground(Color.ORANGE);
 		lblInizioSoggiorno.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblInizioSoggiorno.setBounds(395, 70, 145, 14);
-		frame.getContentPane().add(lblInizioSoggiorno);
+		frmRegistraCliente.getContentPane().add(lblInizioSoggiorno);
 		
 		beginFIeld = new JTextField();
 		beginFIeld.setColumns(10);
 		beginFIeld.setBounds(395, 92, 190, 20);
-		frame.getContentPane().add(beginFIeld);
+		frmRegistraCliente.getContentPane().add(beginFIeld);
 		
 		JLabel lblFineSoggiorno = new JLabel("Fine Soggiorno");
 		lblFineSoggiorno.setForeground(Color.ORANGE);
 		lblFineSoggiorno.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblFineSoggiorno.setBounds(395, 141, 145, 14);
-		frame.getContentPane().add(lblFineSoggiorno);
+		frmRegistraCliente.getContentPane().add(lblFineSoggiorno);
 		
 		endFIeld = new JTextField();
 		endFIeld.setColumns(10);
 		endFIeld.setBounds(395, 163, 190, 20);
-		frame.getContentPane().add(endFIeld);
+		frmRegistraCliente.getContentPane().add(endFIeld);
 		
 		checkoutField = new JTextField();
 		checkoutField.setColumns(10);
 		checkoutField.setBounds(395, 306, 190, 20);
-		frame.getContentPane().add(checkoutField);
+		frmRegistraCliente.getContentPane().add(checkoutField);
 		
 		JLabel lblOrarioCheckout = new JLabel("Orario check-out");
 		lblOrarioCheckout.setForeground(Color.ORANGE);
 		lblOrarioCheckout.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblOrarioCheckout.setBounds(395, 284, 145, 14);
-		frame.getContentPane().add(lblOrarioCheckout);
+		frmRegistraCliente.getContentPane().add(lblOrarioCheckout);
 		
 		checkinFIeld = new JTextField();
 		checkinFIeld.setColumns(10);
 		checkinFIeld.setBounds(395, 235, 190, 20);
-		frame.getContentPane().add(checkinFIeld);
+		frmRegistraCliente.getContentPane().add(checkinFIeld);
 		
 		JLabel lblOrarioCheckin = new JLabel("Orario check-in");
 		lblOrarioCheckin.setForeground(Color.ORANGE);
 		lblOrarioCheckin.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblOrarioCheckin.setBounds(395, 213, 145, 14);
-		frame.getContentPane().add(lblOrarioCheckin);
+		frmRegistraCliente.getContentPane().add(lblOrarioCheckin);
 		
 		JLabel lblTelefono = new JLabel("Telefono");
 		lblTelefono.setForeground(Color.ORANGE);
 		lblTelefono.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblTelefono.setBounds(10, 284, 190, 14);
-		frame.getContentPane().add(lblTelefono);
+		frmRegistraCliente.getContentPane().add(lblTelefono);
 		
 		telField = new JTextField();
 		telField.setColumns(10);
 		telField.setBounds(10, 306, 190, 20);
-		frame.getContentPane().add(telField);
+		frmRegistraCliente.getContentPane().add(telField);
 		
 		JRadioButton rdbtnIntLattosio = new JRadioButton("Int - Lattosio");
 		rdbtnIntLattosio.setForeground(Color.ORANGE);
 		rdbtnIntLattosio.setBackground(Color.DARK_GRAY);
 		rdbtnIntLattosio.setFont(new Font("Verdana", Font.BOLD, 12));
 		rdbtnIntLattosio.setBounds(10, 359, 190, 23);
-		frame.getContentPane().add(rdbtnIntLattosio);
+		frmRegistraCliente.getContentPane().add(rdbtnIntLattosio);
 		
 		rdbtnIntGlutine = new JRadioButton("Int - Glutine");
 		rdbtnIntGlutine.setForeground(Color.ORANGE);
 		rdbtnIntGlutine.setBackground(Color.DARK_GRAY);
 		rdbtnIntGlutine.setFont(new Font("Verdana", Font.BOLD, 12));
 		rdbtnIntGlutine.setBounds(10, 398, 190, 23);
-		frame.getContentPane().add(rdbtnIntGlutine);
+		frmRegistraCliente.getContentPane().add(rdbtnIntGlutine);
 		
 		lblStanzaAssegnata = new JLabel("Stanza assegnata");
 		lblStanzaAssegnata.setForeground(Color.ORANGE);
 		lblStanzaAssegnata.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblStanzaAssegnata.setBounds(395, 359, 145, 14);
-		frame.getContentPane().add(lblStanzaAssegnata);
+		frmRegistraCliente.getContentPane().add(lblStanzaAssegnata);
 		
 		roomFIeld = new JTextField();
 		roomFIeld.setColumns(10);
 		roomFIeld.setBounds(395, 381, 190, 20);
-		frame.getContentPane().add(roomFIeld);
+		frmRegistraCliente.getContentPane().add(roomFIeld);
 		
 		rdbtnIntBB = new JRadioButton("B&B");
 		rdbtnIntBB.setForeground(Color.ORANGE);
 		rdbtnIntBB.setFont(new Font("Verdana", Font.BOLD, 12));
 		rdbtnIntBB.setBackground(Color.DARK_GRAY);
 		rdbtnIntBB.setBounds(642, 92, 190, 23);
-		frame.getContentPane().add(rdbtnIntBB);
+		frmRegistraCliente.getContentPane().add(rdbtnIntBB);
 		
 		rdbtnIntCompleto = new JRadioButton("Completo");
 		rdbtnIntCompleto.setForeground(Color.ORANGE);
 		rdbtnIntCompleto.setFont(new Font("Verdana", Font.BOLD, 12));
 		rdbtnIntCompleto.setBackground(Color.DARK_GRAY);
 		rdbtnIntCompleto.setBounds(642, 118, 190, 23);
-		frame.getContentPane().add(rdbtnIntCompleto);
+		frmRegistraCliente.getContentPane().add(rdbtnIntCompleto);
 		
 		rdbtnIntAllInclusive = new JRadioButton("All-inclusive");
 		rdbtnIntAllInclusive.setForeground(Color.ORANGE);
 		rdbtnIntAllInclusive.setFont(new Font("Verdana", Font.BOLD, 12));
 		rdbtnIntAllInclusive.setBackground(Color.DARK_GRAY);
 		rdbtnIntAllInclusive.setBounds(642, 146, 190, 23);
-		frame.getContentPane().add(rdbtnIntAllInclusive);
+		frmRegistraCliente.getContentPane().add(rdbtnIntAllInclusive);
 		
 		lblTipoSoggiorno = new JLabel("Tipo soggiorno");
 		lblTipoSoggiorno.setForeground(Color.ORANGE);
 		lblTipoSoggiorno.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblTipoSoggiorno.setBounds(643, 71, 145, 14);
-		frame.getContentPane().add(lblTipoSoggiorno);
-		
-		resField = new JTextField();
-		resField.setBounds(642, 235, 159, 20);
-		frame.getContentPane().add(resField);
-		resField.setColumns(10);
-		
-		lblResoconto = new JLabel("Resoconto");
-		lblResoconto.setForeground(Color.MAGENTA);
-		lblResoconto.setFont(new Font("Verdana", Font.BOLD, 12));
-		lblResoconto.setBounds(643, 214, 145, 14);
-		frame.getContentPane().add(lblResoconto);
-		
-		JButton viewPreno = new JButton("Vedi Prenotazioni");
-		viewPreno.setFont(new Font("Verdana", Font.BOLD, 12));
-		viewPreno.setBackground(Color.DARK_GRAY);
-		viewPreno.setForeground(Color.MAGENTA);
-		viewPreno.setBounds(641, 305, 160, 23);
-		frame.getContentPane().add(viewPreno);
+		frmRegistraCliente.getContentPane().add(lblTipoSoggiorno);
 		
 		JButton logout = new JButton("Logout");
 		logout.setForeground(Color.ORANGE);
 		logout.setFont(new Font("Verdana", Font.BOLD, 12));
 		logout.setBackground(Color.DARK_GRAY);
 		logout.setBounds(830, 455, 89, 23);
-		frame.getContentPane().add(logout);
+		frmRegistraCliente.getContentPane().add(logout);
 		
 		JButton backButton = new JButton("Indietro");
 		backButton.setForeground(Color.ORANGE);
 		backButton.setFont(new Font("Verdana", Font.BOLD, 12));
 		backButton.setBackground(Color.DARK_GRAY);
-		backButton.setBounds(732, 455, 89, 23);
-		frame.getContentPane().add(backButton);
+		backButton.setBounds(734, 455, 89, 23);
+		frmRegistraCliente.getContentPane().add(backButton);
 		
-		JButton checkoutButton = new JButton("Checkout");
-		checkoutButton.setForeground(Color.MAGENTA);
-		checkoutButton.setFont(new Font("Verdana", Font.BOLD, 12));
-		checkoutButton.setBackground(Color.DARK_GRAY);
-		checkoutButton.setBounds(732, 398, 187, 46);
-		frame.getContentPane().add(checkoutButton);
-		
-		btnAggiorna = new JButton("Aggiorna");
-		btnAggiorna.setForeground(Color.ORANGE);
-		btnAggiorna.setFont(new Font("Verdana", Font.BOLD, 12));
-		btnAggiorna.setBackground(Color.DARK_GRAY);
-		btnAggiorna.setBounds(642, 257, 159, 23);
-		frame.getContentPane().add(btnAggiorna);
+		JButton registrationButton = new JButton("Registra");
+		registrationButton.setForeground(Color.MAGENTA);
+		registrationButton.setFont(new Font("Verdana", Font.BOLD, 12));
+		registrationButton.setBackground(Color.DARK_GRAY);
+		registrationButton.setBounds(734, 388, 185, 56);
+		frmRegistraCliente.getContentPane().add(registrationButton);
 	}
 }
